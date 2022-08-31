47caf94fb95017a112f39654a5150fa204f6bf0b
YuDaoJun <112454386+YuDaoJun@users.noreply.github.com>
2022-08-31 15:58:17 +0800

    远程仓库修改
    仓库描述

--- src/main/java/com/atguigu/git/Demo.java
+++ src/main/java/com/atguigu/git/Demo.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("1111111111111");
+        System.out.println("远程仓库:2222222222222");
     }
 }
