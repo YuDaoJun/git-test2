c54e7f40f30c45e31a97c3f05ce2c99bdb56daa1
gst <911784100@qq.com>
2022-08-31 17:31:33 +0800

    克隆

--- src/main/java/com/atguigu/git/Demo.java
+++ src/main/java/com/atguigu/git/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("1111111111111");
         System.out.println("远程仓库:2222222222222");
-        System.out.println("克隆仓库:222222222222");
+        System.out.println("克隆仓库:22222222222");
     }
 }
